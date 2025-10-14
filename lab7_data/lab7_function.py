0eb0fad91a22464cf151277121b2bae2e4948812
damion28 <damion28.da@gmail.com>
2025-10-14 15:38:19 +0000

    update lab7, still need to finish exercise and complete examples

--- lab7_data/lab7_function.py
+++ lab7_data/lab7_function.py
@@ -14,3 +14,47 @@ def read_data(filename):
     # use a loop to go over each line in fileuser
     for each in fileuser:
         print(each)
+
+# EXAMPLE 2:
+
+
+# EXAMPLE 3:
+
+
+# EXAMPLE 4:
+
+
+# EXAMPLE 5: loop through a readlines file
+def read_each(filename):
+    with open(filename, "r") as file1:
+        filelines = file1.readlines()
+
+        # loop through each item in 'filelines'
+        for eachline in filelines:
+            print(eachline.strip())
+            # strip() removes the newline character \n
+
+# EXAMPLE 6: create a new file
+def new_file(filename):
+    with open(filename, "w") as file:
+        file.write("Python Basics for data analysis")
+        file.write("Damion Ally")
+
+# EXAMPLE 7: append data into an existing file
+from datetime import datetime
+
+def stamp_date(filename):
+    with open(filename, "a") as file:
+        file.write(f"\n\n{datetime.now()}")
+
+# EXERCISE
+def email_read(filename, email):
+    count_email = 0
+    with open(filename, "r") as file1:
+        filelines = file1.realines()
+        # loop through each item in 'filelines'
+        for eachline in filelines:
+            print(eachline.strip())
+            if # complete the statement
+                # code to increase the count_email
+                return count_email
--- lab7_data/lab7_main.py
+++ lab7_data/lab7_main.py
@@ -8,3 +8,25 @@ from lab7_function import *
 testing()
 print("\n---- Example 1: reading file ----")
 read_data("phrases.txt")
+
+print("\n---- example 2:  ----")
+
+print("\n---- example 3: reading specific portion using readline ----")
+read_readline("phrases.txt")
+
+print("\n---- example 4: reading specific portion using readline ----")
+read_all("phrases.txt")
+
+print("\n---- example 5: loop through each line ----")
+read_each("phrases.txt")
+
+print("\n---- example 6: creating a new file ----")
+new_file("Ally.txt")
+
+print("\n---- example 7: append data into an exisitng file ----")
+stamp_date("Ally.txt")
+
+print("\n---- EXERCISE ----")
+count_yahoo = email_read("user_email.txt","@yahoo")
+count_gmail = email_read("user_email.txt","@gmail")
+count_hotmail = email_read("user_email.txt","@hotmail")
