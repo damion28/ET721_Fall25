87f7558d92017b2872b730d4bf15d010897bd677
damion28 <damion28.da@gmail.com>
2025-10-27 15:35:56 +0000

    intro to Flask

---
+++ lab12_flask/app.py
@@ -0,0 +1,30 @@
+"""
+Damion Ally
+Oct 27, 2025
+lab 12, introduction to flask
+"""
+from flask import Flask, render_template, url_for
+
+"""
+create an object 'app' from the Flask module.
+__name__ set to __main__ if the script is running directly from the main file
+"""
+app = Flask(__name__)
+
+# set the routing to the main(loading) page
+# 'route' decorator is used to access the root URL
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return '<h1>About us</h1>'
+
+@app.route('/quotes')
+def quotes():
+    return '<h1>Quotes</h1>'
+
+# set the 'app' to run if you execute the file directly (not when it is imported)
+if __name__ == "__main__":
+    app.run(debug=True)
