ced5cb3268f821585b057479d95046bb0d5c0f96
damion28 <damion28.da@gmail.com>
2025-11-19 16:38:57 +0000

    to do list with Django

---
+++ dolist/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class Todolisform(forms.Form):
+    text = forms.CharField(max_length=45, widget=forms.TextInput(
+        attrs={
+            'class': 'text-todo',
+            'placeholder': 'Enter to do...',
+            'aria-label': 'Todo',
+            'aria-describedby': 'add-btn'
+        })
+    )
