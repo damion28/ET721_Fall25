d8f64a21da433113e30f282f9b701ddf2a4b72f6
damion28 <damion28.da@gmail.com>
2025-12-01 22:55:58 +0000

    finish lab 16, but still problems with running server

--- lab16_imguploader/imageUploadApp/settings.py
+++ lab16_imguploader/imageUploadApp/settings.py
@@ -122,6 +122,9 @@ import os
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# link to 'static' directory (folder)
+STATICFILES_DIRS = [BASE_DIR/'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
